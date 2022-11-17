93c2b7165702a23587123d4bedbc90b7d9fd88c0
lucasalchapar <54723464+lucasalchapar@users.noreply.github.com>
2022-11-17 16:03:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ global.owner = [
   ['0'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['34605735266'] 
+global.prems = ['34605735266', '34666391671', '656767429']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
