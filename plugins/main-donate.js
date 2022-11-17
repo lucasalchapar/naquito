ff6e2d35a7b9bb6d9cd875c145cd19bdd3b07db7
lucasalchapar <54723464+lucasalchapar@users.noreply.github.com>
2022-11-17 02:14:52 +0200

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -6,11 +6,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 puedes donar si quieres ayudar a mantener el bot activo
 
 ▢ *PayPal*
-• *Link :* https://paypal.me/fg98f
-▢ *Mercado Pago Arg*
-• *Link :* https://mpago.la/1F3r6JH
-▢ *Tigo Money*
-• *N° :*  75140648 
+• *Link :* https://paypal.me/naco17
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
 conn.sendButton(m.chat, don, 'Haga click en boton si quiere comprar o alquilar al bot', img, [['Buy Premium', `${usedPrefix}buyprem`]],m, rpyp)
