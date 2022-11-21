42b9454ba2d8ce2036026696c83ea483077ea379
lucasalchapar <54723464+lucasalchapar@users.noreply.github.com>
2022-11-21 04:37:17 +0200

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -2,17 +2,17 @@
 let handler = async (m, { conn}) => {
 
 let name = conn.getName(m.sender)
-let av = `./src/mp3/${pickRandom(["criss", "andrea"])}.mp3`
+let av = `./src/mp3/${pickRandom(["luu", "trapy"])}.mp3`
 
 conn.sendButton(m.chat, `Hola *${name}* \n \nNecesitas ayuda? \n`, igfg, null, [
       ['⦙☰ Menu', '/help'],
       ['⦙☰ Menu 2', '/menu2'],
-      ['⌬ Grupos', '/gpdylux']
+      ['⌬ Grupos', '/trapy']
     ], m)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|dylux)$/i
+handler.customPrefix = /^(bot|trapy)$/i
 handler.command = new RegExp
 
 export default handler
